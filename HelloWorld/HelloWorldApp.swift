18531f1fe04cac6dd13c5d7270d3fb77b63e4543
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-04-22 06:17:06 +0500

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Bektemur Mamashayev on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
